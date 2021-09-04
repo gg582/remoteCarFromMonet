dd499ecdc4fca22052ee12844c926cb002b36efb
Sunggeun Jin <sunggeun.jin1@gmail.com>
2021-09-04 11:43:54 +0000

    typo in to-car.sh

--- conf-exec.sh
+++ conf-exec.sh
@@ -2,8 +2,8 @@
 
 SONIC_DRV="/dev/car/sr04"
 
-export POD_CAR_ADDR="10.244.1.36"
-export K8S_CAR_ADDR="10.244.5.6"
+export POD_CAR_ADDR="10.244.1.42"
+export K8S_CAR_ADDR="10.244.5.8"
 
 export DOWN_PORT="10101"
 export UP_PORT="10102"
