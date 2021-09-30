f9e2e8b07d33c90b18dc3614f7bb3d308598d17b
Yunjin Lee <yoonjin67@gmail.com>
2021-09-30 12:09:09 +0100

    typo fixed

--- conf-exec.sh
+++ conf-exec.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 
-<<<<<<< HEAD
-export POD_CAR_ADDR="10.244.6.102 n"
-export K8S_CAR_ADDR="10.244.4.124 k8"
-=======
-export POD_CAR_ADDR="10.244.6.102 no"
-export K8S_CAR_ADDR="10.244.4.123 k8s"
->>>>>>> 641f7243211125e38ef2dca9e99c774879d342fa
+export POD_CAR_ADDR="10.244.6.102"
+export K8S_CAR_ADDR="10.244.4.124"
 
 export DOWN_PORT="10101"
 export UP_PORT="10102"
