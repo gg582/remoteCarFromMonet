b05c95f0f49d97b36afc839fba9076fd2f95f385
Yunjin Lee <yoonjin67@gmail.com>
2021-09-30 12:04:19 +0100

    runner changed

--- car/runner/runner.c
+++ car/runner/runner.c
@@ -14,8 +14,8 @@
 
 #include <ioctl_car_cmd.h>
 
-#define AVOID_DIST	200
-#define LOOP_COUNT	100
+#define AVOID_DIST	90
+#define LOOP_COUNT	1000
 #define MOVE_TIME	1
 #define SLEEP_TIME	700
 #define ONE_MILI_SEC	1000	
@@ -65,9 +65,14 @@ int main () {
 
 			ioctl ( dev , PI_CMD_BACKWARD , sizeof ( struct ioctl_info ) );
 			usleep ( 600 * ONE_MILI_SEC ) ;
+			ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+			usleep ( 600 * ONE_MILI_SEC ) ;
 		}
 		else if ( ir1 ) {
 			ioctl ( dev , PI_CMD_LEFT , sizeof ( struct ioctl_info )) ;
+			usleep ( 300 * ONE_MILI_SEC ) ;
+			ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+			usleep ( 600 * ONE_MILI_SEC ) ;
 			while ( ir0 || ir1 ) {
 				ret = read ( ir [ 0 ]  , &ir0 , sizeof ( u_int32_t ) ) ;
 				ret = read ( ir [ 1 ]  , &ir1 , sizeof ( u_int32_t ) ) ;
@@ -77,6 +82,9 @@ int main () {
 			} else if ( ir0 ) {
 		
 				ioctl ( dev , PI_CMD_RIGHT , sizeof ( struct ioctl_info ) );
+				usleep ( 300 * ONE_MILI_SEC ) ;
+				ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+				usleep ( 600 * ONE_MILI_SEC ) ;
 				while ( ir0 || ir1 ) {
 					ret = read ( ir [ 0 ]  , &ir0 , sizeof ( u_int32_t ) ) ;
 					ret = read ( ir [ 1 ]  , &ir1 , sizeof ( u_int32_t ) ) ;
@@ -93,29 +101,39 @@ int main () {
 				ioctl ( dev , PI_CMD_BACKWARD , sizeof ( struct ioctl_info ) );
 				usleep ( 600 * ONE_MILI_SEC ) ;
 				ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+				usleep ( 600 * ONE_MILI_SEC ) ;
 			}
 			else if ( ir1 ) {
 				ioctl ( dev , PI_CMD_LEFT , sizeof ( struct ioctl_info )) ;
+				usleep ( 300 * ONE_MILI_SEC ) ;
+				ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+				usleep ( 600 * ONE_MILI_SEC ) ;
 				while ( ir0 || ir1 ) {
 					ret = read ( ir [ 0 ]  , &ir0 , sizeof ( u_int32_t ) ) ;
 					ret = read ( ir [ 1 ]  , &ir1 , sizeof ( u_int32_t ) ) ;
 				}
-				ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
 				printf ("left --> (%u)\n", value);
 				} else if ( ir0 ) {
 			
 					ioctl ( dev , PI_CMD_RIGHT , sizeof ( struct ioctl_info ) );
+					usleep ( 300 * ONE_MILI_SEC ) ;
+					ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+					usleep ( 600 * ONE_MILI_SEC ) ;
 					while ( ir0 || ir1 ) {
 						ret = read ( ir [ 0 ]  , &ir0 , sizeof ( u_int32_t ) ) ;
 						ret = read ( ir [ 1 ]  , &ir1 , sizeof ( u_int32_t ) ) ;
 					}
 
-					ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
 
+					ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+					usleep ( 600 * ONE_MILI_SEC ) ;
 					printf ("right --> (%u)\n", value);
 
 				} else {
 					ioctl ( dev , PI_CMD_FORWARD , sizeof ( struct ioctl_info ) );
+					usleep ( 200 * ONE_MILI_SEC ) ;
+					ioctl ( dev , PI_CMD_STOP, sizeof ( struct ioctl_info ) );
+					usleep ( 200 * ONE_MILI_SEC ) ;
 				}
 
 
