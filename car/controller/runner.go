c7a574ab9f77d3095ac7f3761b18a78f17f8d60c
Yunjin Lee <yoonjin67@gmail.com>
2021-10-06 12:35:17 +0100

    node1 code updated

--- car/controller/runner.go
+++ car/controller/runner.go
@@ -6,9 +6,225 @@ import (
 	"log"
 	"bufio"
 	"bytes"
-//	"syscall"
+	"../runner/runner"
 	"fmt"
+	"io"
+	"time"
+	"encoding/binary"
+	"syscall"
+	"unsafe"
 )
+
+type ioctlInfo struct {
+
+	size uint32
+	buf [128]byte
+
+}
+
+const (
+
+		AVOID_DIST=100
+		CMD_LEFT = 3
+		CMD_RIGHT = 4
+		CMD_FORWARD = 5
+		CMD_BACKWARD = 6
+		CMD_STOP = 7
+		CMD_GARBAGE = 8
+
+		IR0 = "/dev/car/ir0"
+		IR1 = "/dev/car/ir1"
+		SR04 = "/dev/car/sr04"
+
+	)
+	var devName []string = []string { SR04 , IR0 , IR1 }
+	var devFile map[string](*os.File ) = map[string](*os.File) { SR04 : nil , IR0 : nil , IR1 : nil }
+	var devFd map[string]uintptr = map[string]uintptr { SR04 : 0 , IR0 : 0 , IR1 : 0 }
+
+	var devByte map[string]([]byte)	   = map[string]([]byte)   { SR04 : nil , IR0 : nil , IR1 : nil }
+	var devReader map[string](*bufio.Reader) = map[string](*bufio.Reader) { SR04 : nil , IR0 : nil , IR1 : nil }
+	var devValue map[string]uint32	   = map[string]uint32	   { SR04 : 0 , IR0 : 0 , IR1 : 0 }
+
+
+
+var info ioctlInfo ;
+
+func IoctlMotor ( fd uintptr , cmd int ) {
+
+	syscall.Syscall ( syscall.SYS_IOCTL , fd , uintptr ( cmd ) , uintptr ( unsafe.Pointer ( &info ) ) )
+
+}
+
+
+func main () {
+
+
+
+	var res string
+
+	ch := make ( chan string , 1 )
+
+	go Run ( ch )
+
+	for {
+		log.Println("response res -------> " , res)
+		res = <- ch
+		println ( res )
+	}
+	//download.Close ()
+
+}
+
+func Run ( ch chan string ) {
+
+	motorFile , _ := os.OpenFile ( "/dev/car/motor_tun" , os.O_RDWR , 0775 )
+
+	motor := motorFile.Fd ()
+
+	var btString string
+
+	for _ , name := range devName {
+
+		devFile [ name ] , _ = os.OpenFile ( name , os.O_RDONLY , 0775 )
+
+		devFd [ name ] = devFile [ name ].Fd ()
+
+		devReader [ name ] = bufio.NewReader ( devFile [ name ] )
+
+		devByte [ name ] , _ = io.ReadAll ( devReader [ name ] )
+
+		devByte [ name ] = checkErr ( devByte [ name ] )
+
+		devValue [ name ]    = binary.LittleEndian.Uint32 ( devByte [ name ] )
+
+	}
+
+	for {
+
+		if devValue [ SR04 ] < AVOID_DIST {
+
+			if ( devValue [ IR0 ] == 1 ) && ( devValue [ IR1 ] == 1 ) {
+
+				IoctlMotor ( motor , CMD_BACKWARD )
+
+				time.Sleep ( 800 * time.Millisecond )
+
+				IoctlMotor ( motor , CMD_STOP )
+				btString = `BACKWARD`
+
+			} else if devValue [ IR0 ] == 1 {
+				for ( devValue [ IR0 ] == 1 ) || ( devValue [ IR1 ] == 1 ) {
+
+					IoctlMotor ( motor , CMD_LEFT )
+					devByte [ IR0 ] , _ = io.ReadAll ( devReader [ IR0 ] )
+					devByte [ IR0 ] = checkErr ( devByte [ IR0 ])
+					devValue [ IR0 ] = binary.LittleEndian.Uint32 ( devByte [ IR0 ] )
+					devByte [ IR1 ] , _ = io.ReadAll ( devReader [ IR1 ] )
+					devByte [ IR1 ] = checkErr ( devByte [ IR1 ] )
+					devValue [ IR1 ] = binary.LittleEndian.Uint32 ( devByte [ IR1 ] )
+				}
+
+				btString = `LEFT`
+				IoctlMotor ( motor , CMD_STOP )
+
+			} else if devValue [ IR1 ] == 1{
+
+				for ( devValue [ IR0 ] == 1 ) || ( devValue [ IR1 ] == 1 ) {
+
+					IoctlMotor ( motor , CMD_RIGHT )
+					devByte [ IR0 ] , _ = io.ReadAll ( devReader [ IR0 ] )
+					devByte [ IR0 ] = checkErr ( devByte [ IR0 ])
+					devValue [ IR0 ] = binary.LittleEndian.Uint32 ( devByte [ IR0 ] )
+					devByte [ IR1 ] , _ = io.ReadAll ( devReader [ IR1 ] )
+					devByte [ IR1 ] = checkErr ( devByte [ IR1 ] )
+					devValue [ IR1 ] = binary.LittleEndian.Uint32 ( devByte [ IR1 ] )
+
+				}
+
+
+				IoctlMotor ( motor , CMD_STOP )
+				btString = `RIGHT`
+			} else {
+				if ( devValue [ IR0 ] == 1 ) && ( devValue [ IR1 ] == 1 ) {
+
+					IoctlMotor ( motor , CMD_BACKWARD )
+
+					time.Sleep ( 800 * time.Millisecond )
+
+					IoctlMotor ( motor , CMD_STOP )
+
+					btString = `BACKWARD`
+
+				} else if devValue [ IR0 ] == 1 {
+					for ( devValue [ IR0 ] == 1 ) || ( devValue [ IR1 ] == 1 ) {
+
+						IoctlMotor ( motor , CMD_LEFT )
+						devByte [ IR0 ] , _ = io.ReadAll ( devReader [ IR0 ] )
+						devByte [ IR0 ] = checkErr ( devByte [ IR0 ])
+						devValue [ IR0 ] = binary.LittleEndian.Uint32 ( devByte [ IR0 ] )
+						devByte [ IR1 ] , _ = io.ReadAll ( devReader [ IR1 ] )
+						devByte [ IR1 ] = checkErr ( devByte [ IR1 ] )
+						devValue [ IR1 ] = binary.LittleEndian.Uint32 ( devByte [ IR1 ] )
+					}
+
+					btString = `LEFT`
+					IoctlMotor ( motor , CMD_STOP )
+
+				} else if devValue [ IR1 ] == 1{
+
+					for ( devValue [ IR0 ] == 1 ) || ( devValue [ IR1 ] == 1 ) {
+
+						IoctlMotor ( motor , CMD_RIGHT )
+						devByte [ IR0 ] , _ = io.ReadAll ( devReader [ IR0 ] )
+						devByte [ IR0 ] = checkErr ( devByte [ IR0 ])
+						devValue [ IR0 ] = binary.LittleEndian.Uint32 ( devByte [ IR0 ] )
+						devByte [ IR1 ] , _ = io.ReadAll ( devReader [ IR1 ] )
+						devByte [ IR1 ] = checkErr ( devByte [ IR1 ] )
+						devValue [ IR1 ] = binary.LittleEndian.Uint32 ( devByte [ IR1 ] )
+
+					}
+					btString = `RIGHT`
+
+				} else {
+
+					IoctlMotor ( motor , CMD_STOP )
+
+					IoctlMotor ( motor , CMD_FORWARD )
+
+					time.Sleep ( 700 * time.Millisecond )
+
+					IoctlMotor ( motor , CMD_STOP )
+
+					btString = `FORWARD`
+				}
+			}
+
+			log.Println( "Step 4")
+
+			ch <- btString
+
+			log.Println( "Step 5")
+		}
+
+	}
+
+}
+
+func checkErr ( byteSlice []byte ) []byte {
+
+		if len ( byteSlice ) != 4 {
+
+
+			for i := 0 ; i < 4 ; i ++ {
+
+				byteSlice = append ( byteSlice , 0 )
+
+			}
+
+		}
+		return byteSlice
+}
+
 func main () {
 
 	if len ( os.Args ) < 2 {
@@ -47,6 +263,10 @@ func main () {
 }
 
 func Run ( download net.Conn , ch chan string , PATH string ) {
+	motorFile := os.OpenFile ( "/dev/car/motor_tun" , os.O_RDWR , 0775 )
+
+
+	motor := motorFile.Fd ()
 	for {
 		log.Println( "Step 1")
 
@@ -66,21 +286,21 @@ func Run ( download net.Conn , ch chan string , PATH string ) {
 		switch ( btString ) {
 			case "LEFT" :
 				log.Println( "Step 3" )
-				execute (  PATH+"/"+"left"  )
+				IoctlMotor (  motor , CMD_LEFT  )
 			case "RGHT" :
-				execute (  PATH+"/"+"right"  )
+				IoctlMotor (  motor , CMD_RIGHT  )
 			case "FWRD" :
 				log.Println( "switch parameter ====>" , btString )
-				execute (  PATH+"/"+"forward"  )
+				IoctlMotor (  motor , CMD_FORWARD  )
 			case "BKWD" :
-				execute (  PATH+"/"+"backward"  )
+				IoctlMotor (  motor , CMD_BACKWARD  )
 			case "STOP" :
-				execute (  PATH+"/"+"stop"  )
+				IoctlMotor (  motor , CMD_STOP )
 			case "TERM" :
-				execute (  PATH+"/"+"stop"  )
+				IoctlMotor ( motor , CMD_STOP )
 				os.Exit ( 1 ) 
 			case "GABG" :
-				execute ( PATH+"/"+"garbage"  )
+				IoctlMotor ( motor , CMD_GARBAGE  )
 		}
 		log.Println( "Step 4")
 
@@ -89,17 +309,4 @@ func Run ( download net.Conn , ch chan string , PATH string ) {
 	}
 }
 
-func execute ( cmd string  ) {
-	//err := syscall.Exec ( cmd , nil , os.Environ () )
-	log.Println( "6" )
-	res, err := exec.Command ( cmd ).Output ( )
-	if err != nil {
-
-		log.Println ( err )
-
-	}
-	log.Println("res from run", res )
-
-
-}
 
--- conf-exec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-export POD_CAR_ADDR="10.244.6.102"
-export K8S_CAR_ADDR="10.244.4.124"
-
-export DOWN_PORT="10101"
-export UP_PORT="10102"
