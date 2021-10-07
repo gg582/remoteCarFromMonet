146c9db14df1db3d2b73da2d679fc61bd25b5248
Yunjin Lee <yoonjin67@gmail.com>
2021-10-07 03:29:45 +0100

    send function changed

--- car/controller/sendSensorValueToVirtCar.go
+++ car/controller/sendSensorValueToVirtCar.go
@@ -140,6 +140,10 @@ func READ ( uport net.Conn , ch chan bool ) {
 		}
 		byte1 = append ( byte1 , byte ( '\n' ) )
 		fmt.Printf ( "sr04:%d ir0:%d ir1:%d \n" , car.Sr04Val , car.Ir0Val , car.Ir1Val )
+		if ( car.Sr04Val == 0 ) {
+			ch <- false
+			continue
+		}
 
 		_ , err = uport.Write ( byte1 )
 
